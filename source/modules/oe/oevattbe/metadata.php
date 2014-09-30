c957ddef278b29cc313605cfca996c40f657f343
l3k <linas.kukulskis@gmail.com>
2014-09-30 10:38:47 +0300

    Structure.

---
+++ source/modules/oe/oevattbe/metadata.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of OXID eSales PayPal module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales PayPal module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.2';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'           => 'oevattbe',
+    'title'        => 'VAT TBE services',
+    'description'  => array(
+        'de' => 'Module for VAT TBE Services.',
+        'en' => 'Module for VAT TBE Services.',
+    ),
+    'thumbnail'    => 'logo.jpg',
+    'version'      => '1.0.0',
+    'author'       => 'OXID eSales AG',
+    'url'          => 'http://www.oxid-esales.com',
+    'email'        => 'info@oxid-esales.com',
+    'extend'       => array(
+
+    ),
+    'files' => array(
+    ),
+    'events'       => array(
+        'onActivate'   => 'oeVatTbeEvents::onActivate',
+        'onDeactivate' => 'oeVatTbeEvents::onDeactivate'
+    ),
+    'templates' => array(
+    ),
+    'blocks' => array(
+
+    ),
+    'settings' => array(
+
+    )
+);
