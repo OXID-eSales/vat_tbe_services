9c21389a937959937f20f5c36c80dcc3d7d64611
mantasvaitkunas <manvait@gmail.com>
2014-10-02 16:47:40 +0300

    Changed class name

--- tests/acceptance/oevattbe/dummyTest.php
+++ tests/acceptance/oevattbe/dummyTest.php
@@ -24,7 +24,7 @@ require_once realpath( "." ) . '/acceptance/oxTestCase.php';
 /**
  * Acceptance test cases
  */
-class Acceptance_oeVATTBE_dummyTest extends oxTestCase
+class oeVATTBE_dummyTest extends oxTestCase
 {
 
     const TEST_USER_NAME = 'test@oxid-esales.com';
