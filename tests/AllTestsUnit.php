dc930fc6904b40f676fa4d9aa640d322f655af9e
mantasvaitkunas <manvait@gmail.com>
2014-10-02 14:54:09 +0300

    Changed header

--- tests/AllTestsUnit.php
+++ tests/AllTestsUnit.php
@@ -1,13 +1,22 @@
 <?php
 /**
- * This file contains the script required to run all PE edition unit tests in unit dir on Cruise Control.
- * This file is supposed to be executed over PHPUnit framework
- * It is called something like this:
- * phpunit <Test dir>_AllTests
+ * This file is part of OXID eSales VAT TBE module.
  *
- * @link          http://www.oxid-esales.com
- * @copyright (c) OXID eSales AG 2003-#OXID_VERSION_YEAR#
- * @version       SVN: $Id: $
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales VAT TBE module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
  */
 
 require_once 'AllTestsRunner.php';
