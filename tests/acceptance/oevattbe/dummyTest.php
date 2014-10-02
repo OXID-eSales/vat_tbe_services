6448a70a53cf3bfbc9ae62f2ce5144833855c3cc
mantasvaitkunas <manvait@gmail.com>
2014-10-02 14:52:01 +0300

    Adding dummy selenium test

---
+++ tests/acceptance/oevattbe/dummyTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is part of OXID eSales VAT TBE module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales VAT TBE module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ */
+
+require_once realpath( "." ) . '/acceptance/library/oxTestCase.php';
+
+/**
+ * Acceptance test cases
+ */
+class Acceptance_oeMobileTheme_mobileTest extends oxTestCase
+{
+
+    const TEST_USER_NAME = 'test@oxid-esales.com';
+    const TEST_USER_PASSWORD = 'testtest';
+
+    /**
+     * test for activating Module.
+     *
+     * @group mobile
+     */
+    public function testActivateExtension()
+    {
+        $this->open(shopURL . "admin");
+        $this->loginAdminForModule("Extensions", "Themes", null, null, null, "admin@myoxideshop.com", "admin0303");
+        $this->openListItem("link=OXID eShop mobile theme");
+        $this->clickAndWait("//input[@value='Activate']");
+        $this->selectMenu("Extensions", "Modules");
+        $this->openListItem("link=OXID eShop theme switch");
+        $this->clickAndWait("module_activate");
+        // dumping database
+        try {
+            $this->dumpDB();
+        } catch (Exception $e) {
+            $this->stopTesting("Failed dumping original db");
+        }
+    }
+}
