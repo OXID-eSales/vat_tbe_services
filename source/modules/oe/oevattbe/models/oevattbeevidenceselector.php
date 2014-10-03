7e65da58fd101001b3f812997f13193346f83814
shiftas <Shiftass@gmail.com>
2014-10-03 14:45:16 +0300

    OEVATTBE-7 Create evidence calculator
    Adding evidence selector. It selects default evidence or any other in a row if default selector has no value.

---
+++ source/modules/oe/oevattbe/models/oevattbeevidenceselector.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * This file is part of OXID eSales VAT TBE module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales VAT TBE module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014T
+ */
+
+
+/**
+ * Class checks all collected evidences and provides user country from them.
+ */
+class oeVATTBEEvidenceSelector
+{
+    /** @var array List of evidence collectors */
+    private $_oEvidenceList = array();
+
+    /** @var array List of evidence collectors */
+    private $_oConfig = array();
+
+    /**
+     * Class constructor method, handles required dependencies.
+     *
+     * @param oeVATTBEEvidenceList $oEvidenceList List of evidences.
+     * @param oxConfig             $oConfig    Shop Config object.
+     */
+    public function __construct($oEvidenceList, $oConfig)
+    {
+        $this->_oEvidenceList = $oEvidenceList;
+        $this->_oConfig = $oConfig;
+    }
+
+    /**
+     * Checks all evidences and returns country from them.
+     *
+     * @return string
+     */
+    public function getEvidence()
+    {
+        $oConfig = $this->_getConfig();
+        $sDefaultEvidence = $oConfig->getConfigParam('sDefaultTBEEvidence');
+
+        $oEvidenceList = $this->_getEvidenceList();
+
+        $oFirstNonEmptyEvidence = null;
+        $oDefaultEvidence = null;
+        foreach ($oEvidenceList as $oEvidence) {
+            /** @var oeVATTBEEvidence $oEvidence */
+            if ($sDefaultEvidence == $oEvidence->getName()) {
+                $oDefaultEvidence = $oEvidence;
+            }
+            if (!$oFirstNonEmptyEvidence && $oEvidence->getCountry()) {
+                $oFirstNonEmptyEvidence = $oEvidence;
+            }
+        }
+
+        return $oDefaultEvidence ? $oDefaultEvidence : $oFirstNonEmptyEvidence;
+    }
+
+    /**
+     * Checks if all countries are unique.
+     *
+     * @return bool
+     */
+    public function isEvidencesContradicting()
+    {
+        $aEvidences = $this->_getEvidenceList();
+
+        $aUniqueCountries = array();
+        foreach ($aEvidences as $oEvidence) {
+            /** @var oeVATTBEEvidence $oEvidence */
+            $aUniqueCountries[$oEvidence->getCountry()] = $oEvidence->getCountry();
+        }
+
+        return (count($aUniqueCountries) === 1) ? false : true;
+    }
+
+    /**
+     * Returns evidence list.
+     *
+     * @return oeVATTBEEvidenceList
+     */
+    protected function _getEvidenceList()
+    {
+        return $this->_oEvidenceList;
+    }
+
+    /**
+     * Returns evidence list.
+     *
+     * @return oxConfig
+     */
+    protected function _getConfig()
+    {
+        return $this->_oConfig;
+    }
+}
---
+++ tests/unit/oevattbe/models/oevattbeevidenceselectorTest.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * This file is part of OXID eSales VAT TBE module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales VAT TBE module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014T
+ */
+
+/**
+ * Test class for oeVATTBEEvidenceCalculator.
+ *
+ * @covers oeVATTBEEvidenceSelector
+*/
+class Unit_oeVATTBE_Models_oeVATTBEEvidenceSelectorTest extends OxidTestCase
+{
+
+
+    public function providerGetCountryWhenBothEvidenceDoNotMatch()
+    {
+        $oBillingEvidence = $this->_createEvidence('billing_address', 'Germany');
+        $oGeoLocationEvidence = $this->_createEvidence('geo_location', 'Lithuania');
+        $oEvidenceList = new oeVATTBEEvidenceList(array($oBillingEvidence, $oGeoLocationEvidence));
+
+        return array(
+            array($oEvidenceList, 'billing_address', $oBillingEvidence),
+            array($oEvidenceList, 'geo_location', $oGeoLocationEvidence)
+        );
+    }
+
+    /**
+     * @param oeVATTBEEvidenceList $oEvidenceList
+     * @param string               $sDefaultEvidence
+     * @param oeVATTBEEvidence     $sExpectedEvidence
+     *
+     * @dataProvider providerGetCountryWhenBothEvidenceDoNotMatch
+     */
+    public function testGetCountryWhenBothEvidenceDoNotMatchDefaultTaken($oEvidenceList, $sDefaultEvidence, $sExpectedEvidence)
+    {
+        $oConfig = $this->getConfig();
+        $oConfig->setConfigParam('sDefaultTBEEvidence', $sDefaultEvidence);
+
+        $oCalculator = new oeVATTBEEvidenceSelector($oEvidenceList, $oConfig);
+
+        $this->assertSame($sExpectedEvidence, $oCalculator->getEvidence());
+    }
+
+    public function testGetCountryWhenDefaultEvidenceEmpty()
+    {
+        $oConfig = $this->getConfig();
+        $oConfig->setConfigParam('sDefaultTBEEvidence', 'default_evidence');
+
+        $oBillingEvidence = $this->_createEvidence('billing_address', 'Germany');
+        $oGeoLocationEvidence = $this->_createEvidence('geo_location', 'Lithuania');
+        $oDefaultEvidence = $this->_createEvidence('default_evidence', '');
+        $oEvidenceList = new oeVATTBEEvidenceList(array($oBillingEvidence, $oGeoLocationEvidence, $oDefaultEvidence));
+
+        $oCalculator = new oeVATTBEEvidenceSelector($oEvidenceList, $oConfig);
+
+        $this->assertEquals($oBillingEvidence, $oCalculator->getEvidence());
+    }
+
+    public function testGetCountryWhenDefaultAndFirstEvidenceEmpty()
+    {
+        $oConfig = $this->getConfig();
+        $oConfig->setConfigParam('sDefaultTBEEvidence', 'default_evidence');
+
+        $oBillingEvidence = $this->_createEvidence('billing_address', '');
+        $oGeoLocationEvidence = $this->_createEvidence('geo_location', 'Lithuania');
+        $oDefaultEvidence = $this->_createEvidence('default_evidence', '');
+        $oEvidenceList = new oeVATTBEEvidenceList(array($oBillingEvidence, $oGeoLocationEvidence, $oDefaultEvidence));
+
+        $oCalculator = new oeVATTBEEvidenceSelector($oEvidenceList, $oConfig);
+
+        $this->assertEquals($oGeoLocationEvidence, $oCalculator->getEvidence());
+    }
+
+    public function testIsEvidencesContradictingWhenEvidencesDoNotMatch()
+    {
+        $oBillingEvidence = $this->_createEvidence('billing_address', 'Germany');
+        $oGeoLocationEvidence = $this->_createEvidence('geo_location', 'Germany');
+        $oEvidenceList = new oeVATTBEEvidenceList(array($oBillingEvidence, $oGeoLocationEvidence));
+
+        $oCalculator = new oeVATTBEEvidenceSelector($oEvidenceList, $this->getConfig());
+
+        $this->assertEquals(false, $oCalculator->isEvidencesContradicting());
+    }
+
+    public function testIsEvidencesContradictingWhenEvidencesMatch()
+    {
+        $oBillingEvidence = $this->_createEvidence('billing_address', 'Germany');
+        $oGeoLocationEvidence = $this->_createEvidence('geo_location', 'Lithuania');
+        $oEvidenceList = new oeVATTBEEvidenceList(array($oBillingEvidence, $oGeoLocationEvidence));
+
+        $oCalculator = new oeVATTBEEvidenceSelector($oEvidenceList, $this->getConfig());
+
+        $this->assertEquals(true, $oCalculator->isEvidencesContradicting());
+    }
+
+    /**
+     * Creates evidence object with given name and country.
+     *
+     * @param string $sName
+     * @param string $sCountry
+     *
+     * @return oeVATTBEEvidence
+     */
+    protected function _createEvidence($sName, $sCountry)
+    {
+        /** @var oeVATTBEEvidence|PHPUnit_Framework_MockObject_MockObject $oUser */
+        $oEvidence = $this->getMock('oeVATTBEEvidence', array('getName', 'getCountry'), array(), '', false);
+        $oEvidence->expects($this->any())->method('getName')->will($this->returnValue($sName));
+        $oEvidence->expects($this->any())->method('getCountry')->will($this->returnValue($sCountry));
+
+        return $oEvidence;
+    }
+}
