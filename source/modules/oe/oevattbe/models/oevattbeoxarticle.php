2ca33c437cdf7f96b8c723d2cbd01119d0ab4d50
l3k <linas.kukulskis@gmail.com>
2014-10-03 12:55:59 +0300

    OEVATTBE-10 Update oxArticle to select TBE VAT: article load

--- source/modules/oe/oevattbe/models/oevattbeoxarticle.php
+++ source/modules/oe/oevattbe/models/oevattbeoxarticle.php
@@ -24,5 +24,102 @@
  */
 class oeVatTbeOxArticle extends oeVatTbeOxArticle_parent
 {
+    /**
+     * Builds and returns SQL query string.
+     *
+     * @param mixed $aWhere SQL select WHERE conditions array (default false)
+     *
+     * @return string
+     */
+    public function buildArticleSelect($aWhere = null)
+    {
+        $sSelect = "SELECT " . $this->getSelectFields();
+        $sSelect .= " , `oevattbe_countryvatgroups`.`OEVATTBE_RATE` ";
+        $sSelect .= " FROM " . $this->getViewName();
+        $sSelect .= " LEFT JOIN `oevattbe_articlevat` ON `".$this->getViewName()."`.`oxid` = `oevattbe_articlevat`.`OEVATTBE_ARTICLEID` ";
+        $sSelect .= " AND `oevattbe_articlevat`.`OEVATTBE_COUNTRYID` = " . oxDb::getDb()->quote($this->_getTbeCountryId());
+        $sSelect .= " LEFT JOIN `oevattbe_countryvatgroups` ON `oevattbe_articlevat`.`OEVATTBE_VATGROUPID` = `oevattbe_countryvatgroups`.`OEVATTBE_ID` ";
+        $sSelect .= " WHERE 1 ";
 
+        if ($aWhere) {
+            reset($aWhere);
+            while (list($name, $value) = each($aWhere)) {
+                $sSelect .= ' and ' . $name . ' = ' . oxDb::getDb()->quote($value);
+            }
+        }
+
+        // add active shop
+        if (oxRegistry::getConfig()->getEdition() == 'EE') {
+            if ($this->getShopId() && $this->_blDisableShopCheck === false) {
+                $sLongFieldName = $this->_getFieldLongName('oxshopid');
+                if (isset($this->$sLongFieldName)) {
+                    $sFieldName = $this->getViewName() . ".oxshopid";
+                    if (!isset($aWhere[$sFieldName])) {
+                        $sSelect .= " and $sFieldName = '" . $this->getShopId() . "'";
+                    }
+                }
+            }
+        }
+
+        return $sSelect;
+    }
+
+    /**
+     * Get data from db
+     *
+     * @param string $sOxId id
+     *
+     * @return array
+     */
+    protected function _loadFromDb($sOxId)
+    {
+        $sTbeCountry = $this->_getTbeCountryId();
+
+        if (!is_null($sTbeCountry)) {
+            if (oxRegistry::getConfig()->getEdition() == 'EE') {
+                $blCoreTableUsage = $this->getForceCoreTableUsage();
+                $this->_forceCoreTableUsageForSharedBasket();
+            }
+
+            $sSelect = $this->buildArticleSelect(array($this->getViewName() . ".oxid" => $sOxId));
+
+            if (oxRegistry::getConfig()->getEdition() == 'EE') {
+                $this->setForceCoreTableUsage($blCoreTableUsage);
+            }
+            $aData = oxDb::getDb(oxDb::FETCH_MODE_ASSOC)->getRow($sSelect);
+        } else {
+            $aData = parent::_loadFromDb($sOxId);
+        }
+
+        return $aData;
+    }
+
+
+    /**
+     * Returns users tbe country
+     *
+     * @return string
+     */
+    private function _getTbeCountryId()
+    {
+        $sCountryId = null;
+        $oUser = $this->getUser();
+
+        if ($oUser) {
+            $sCountryId = $oUser->getTbeCountryId();
+        }
+
+        return $sCountryId;
+    }
+
+
+    /**
+     * Sets forcing of core table usage for creating table view name when shared basket is enabled.
+     */
+    private function _forceCoreTableUsageForSharedBasket()
+    {
+        if ($this->getConfig()->getConfigParam('blMallSharedBasket')) {
+            $this->setForceCoreTableUsage(true);
+        }
+    }
 }
---
+++ tests/unit/oevattbe/models/oevattbeoxarticleTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * This file is part of OXID eSales VAT TBE module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales VAT TBE module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ */
+
+
+/**
+ * Testing extended oxArticle class.
+ */
+class Unit_oeVatTbe_models_oeVatTbeOxArticleTest extends OxidTestCase
+{
+
+    /**
+     * Test case for loading article
+     */
+    public function testLoadArticle()
+    {
+        $this->_prepareData();
+
+        $oUser = $this->getMock("oxUser", array("getTbeCountryId"));
+        $oUser->expects($this->any())->method("getTbeCountryId")->will($this->returnValue('a7c40f631fc920687.20179984'));
+
+        $oArticle = oxNew('oxArticle');
+        $oArticle->setUser($oUser);
+
+        $oArticle->load('1126');
+
+        $this->assertSame('8.00', $oArticle->oxarticles__oevattbe_rate->value);
+    }
+
+    /**
+     * Test case for loading article
+     */
+    public function testLoadArticleNoTbeCountry()
+    {
+        $this->_prepareData();
+
+        $oUser = $this->getMock("oxUser", array("getTbeCountryId"));
+        $oUser->expects($this->any())->method("getTbeCountryId")->will($this->returnValue(null));
+
+        $oArticle = oxNew('oxArticle');
+        $oArticle->setUser($oUser);
+
+        $oArticle->load('1126');
+
+        $this->assertNull($oArticle->oxarticles__oevattbe_rate->value);
+    }
+
+
+    /**
+     * prepare data
+     */
+    protected function _prepareData()
+    {
+        $oDb = oxDb::getDb();
+
+        $oDb->execute("TRUNCATE TABLE oevattbe_countryvatgroups");
+        $oDb->execute("TRUNCATE TABLE oevattbe_articlevat");
+
+        $sql = "INSERT INTO oevattbe_countryvatgroups SET OEVATTBE_ID = 1, OEVATTBE_COUNTRYID = 'a7c40f631fc920687.20179984', OEVATTBE_NAME='name', OEVATTBE_RATE='8'";
+
+        $oDb->execute($sql);
+
+        $sql = "INSERT INTO oevattbe_articlevat SET OEVATTBE_ARTICLEID = '1126', OEVATTBE_COUNTRYID = 'a7c40f631fc920687.20179984', OEVATTBE_VATGROUPID = '1'";
+
+        $oDb->execute($sql);
+    }
+}
+
