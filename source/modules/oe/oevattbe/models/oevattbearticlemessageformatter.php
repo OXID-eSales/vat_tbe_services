d200b0fd71dd9a236939abba257dbf34013d091d
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-10-08 17:39:21 +0300

    OEVATTBE-16 Create connecting layer for oxBasket for checking its articles.
    Added formatter for error message if there are articles with wrong TBE VAT

---
+++ source/modules/oe/oevattbe/models/oevattbearticlemessageformatter.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of OXID eSales VAT TBE module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales VAT TBE module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014T
+ */
+
+/**
+ * Forms error message if there are articles with invalid TBE VAT.
+ */
+class oeVATTBEArticleMessageFormatter
+{
+    /**
+     * Forms error message for articles with TBE VAT problems.
+     *
+     * @param array $oInvalidArticles Takes titles to form error message.
+     *
+     * @return oxDisplayError
+     */
+    public function getMessage($oInvalidArticles)
+    {
+        /** @var oxDisplayError $oEx */
+        $oEx = oxNew('oxDisplayError');
+        /** @var oxLang $oLang */
+        $oLang = oxRegistry::getLang();
+
+        /** @var oxArticle $oInvalidArticle */
+        foreach ($oInvalidArticles as $oInvalidArticle) {
+            $aArticleNames[] = $oInvalidArticle->oxarticles__oxtitle->value;
+        }
+        $sArticleNames = implode(', ', $aArticleNames);
+
+        $oEx->setMessage(sprintf($oLang->translateString('ERROR_MESSAGE_TBE_ARTICLE_VAT_PROBLEMS', $oLang->getTplLanguage()), $sArticleNames));
+
+        return $oEx;
+    }
+}
