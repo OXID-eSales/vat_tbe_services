48d77e243766ee913ee32ac5b5aae3e71ecf90d9
shiftas <Shiftass@gmail.com>
2014-10-17 10:25:31 +0300

    OEVATTBE-75 Make module certifiabale
    Refactoring chains in Unit_oeVatTbe_models_oeVATTBETBEUserTest. Also added missing comments.

--- tests/unit/oevattbe/models/oevattbetbeuserTest.php
+++ tests/unit/oevattbe/models/oevattbetbeuserTest.php
@@ -27,6 +27,10 @@ class Unit_oeVatTbe_models_oeVATTBETBEUserTest extends OxidTestCase
 {
     private $_hasDependencies = false;
 
+    /**
+     * Initiates tearDown for tests which does not have dependencies.
+     * Test have to define $this->_hasDependencies = true; if no tearDown should be done.
+     */
     public function tearDown()
     {
         if (!$this->_hasDependencies) {
@@ -35,11 +39,16 @@ class Unit_oeVatTbe_models_oeVATTBETBEUserTest extends OxidTestCase
         $this->_hasDependencies = false;
     }
 
+    /**
+     * Tests collecting of TBE evidences when evidence collector is billing country and it is set as default.
+     *
+     * @return oeVATTBETBEUser
+     */
     public function testCollectingTBEEvidenceList()
     {
         $oConfig = $this->getConfig();
         $oSession = $this->getSession();
-        $oConfig->setConfigParam('blOeVATTBECountryEvidences', array('oeVATTBEBillingCountryEvidence'));
+        $oConfig->setConfigParam('aOeVATTBECountryEvidences', array('oeVATTBEBillingCountryEvidence'));
         $oConfig->setConfigParam('sOeVATTBEDefaultEvidence', 'billing_country');
 
         $oUser = oxNew('oxUser');
@@ -54,35 +63,49 @@ class Unit_oeVatTbe_models_oeVATTBETBEUserTest extends OxidTestCase
             ),
         );
         $this->assertEquals($aExpected, $oTBEUser->getTBEEvidenceList());
+        $this->_hasDependencies = true;
 
         return $oTBEUser;
     }
 
     /**
+     * Test selection of country id from evidence list.
+     *
      * @param oeVATTBETBEUser $oTBEUser
      *
      * @depends testCollectingTBEEvidenceList
+     *
+     * @return oeVATTBETBEUser
      */
     public function testTBECountryIdSelecting($oTBEUser)
     {
         $this->assertEquals('GermanyId', $oTBEUser->getTbeCountryId());
+        $this->_hasDependencies = true;
+        return $oTBEUser;
     }
 
     /**
+     * Test if correct evidence is used for selecting user country from evidence list.
+     *
      * @param oeVATTBETBEUser $oTBEUser
      *
-     * @depends testCollectingTBEEvidenceList
+     * @depends testTBECountryIdSelecting
      */
     public function testTBEEvidenceUsedSelecting($oTBEUser)
     {
         $this->assertEquals('billing_country', $oTBEUser->getTbeEvidenceUsed());
     }
 
+    /**
+     * Tests collecting of evidences when no evidence collectors are registered.
+     *
+     * @return oeVATTBETBEUser
+     */
     public function testCollectingOfTBEEvidenceListWhenEvidenceListIsEmpty()
     {
         $oConfig = $this->getConfig();
         $oSession = $this->getSession();
-        $oConfig->setConfigParam('blOeVATTBECountryEvidences', array());
+        $oConfig->setConfigParam('aOeVATTBECountryEvidences', array());
         $oConfig->setConfigParam('sOeVATTBEDefaultEvidence', '');
 
         $oUser = oxNew('oxUser');
@@ -90,45 +113,60 @@ class Unit_oeVatTbe_models_oeVATTBETBEUserTest extends OxidTestCase
         $oTBEUser = oxNew('oeVATTBETBEUser', $oUser, $oSession, $oConfig);
 
         $this->assertEquals(array(), $oTBEUser->getTBEEvidenceList());
-
+        $this->_hasDependencies = true;
         return $oTBEUser;
     }
 
     /**
+     * Tests selecting of evidence when no evidences are found.
+     *
      * @param oeVATTBETBEUser $oTBEUser
      *
      * @depends testCollectingOfTBEEvidenceListWhenEvidenceListIsEmpty
+     *
+     * @return oeVATTBETBEUser
      */
     public function testTBECountryIdSelectingWhenNoEvidenceFound($oTBEUser)
     {
         $this->assertEquals('', $oTBEUser->getTbeCountryId());
+        $this->_hasDependencies = true;
+        return $oTBEUser;
     }
 
     /**
+     * Tests returning of evidence collector used for selecting user country when no evidences are found.
+     *
      * @param oeVATTBETBEUser $oTBEUser
      *
-     * @depends testCollectingOfTBEEvidenceListWhenEvidenceListIsEmpty
+     * @depends testTBECountryIdSelectingWhenNoEvidenceFound
      */
     public function testTBEEvidenceUsedSelectingWhenNoEvidenceFound($oTBEUser)
     {
         $this->assertEquals('', $oTBEUser->getTbeEvidenceUsed());
     }
 
+    /**
+     * Test evidence list caching. Second time evidence list is returned, it should not be recalculated.
+     * Evidences must be stored in session, not in local cache.
+     *
+     * @return oeVATTBETBEUser
+     */
     public function testTBEEvidenceListCaching()
     {
         $oConfig = $this->getConfig();
         $oSession = $this->getSession();
         $oSession->setVariable('TBECountryId', null);
-        $oConfig->setConfigParam('blOeVATTBECountryEvidences', array('oeVATTBEBillingCountryEvidence'));
+        $oConfig->setConfigParam('aOeVATTBECountryEvidences', array('oeVATTBEBillingCountryEvidence'));
         $oConfig->setConfigParam('sOeVATTBEDefaultEvidence', 'billing_country');
 
         $oUser = oxNew('oxUser');
         $oUser->oxuser__oxcountryid = new oxField('GermanyId');
 
+        /** @var oeVATTBETBEUser $oTBEUser */
         $oTBEUser = oxNew('oeVATTBETBEUser', $oUser, $oSession, $oConfig);
         $oTBEUser->getTBEEvidenceList();
 
-        $oConfig->setConfigParam('blOeVATTBECountryEvidences', array('oeVATTBEGeoLocationEvidence'));
+        $oConfig->setConfigParam('aOeVATTBECountryEvidences', array('oeVATTBEGeoLocationEvidence'));
         $oConfig->setConfigParam('sOeVATTBEDefaultEvidence', 'geo_location');
         $oUser->oxuser__oxcountryid = new oxField('LithuaniaId');
 
@@ -138,14 +176,19 @@ class Unit_oeVatTbe_models_oeVATTBETBEUserTest extends OxidTestCase
                 'countryId' => 'GermanyId'
             )
         );
+
+        /** @var oeVATTBETBEUser $oTBEUser */
+        $oTBEUser = oxNew('oeVATTBETBEUser', $oUser, $oSession, $oConfig);
         $this->assertEquals($aExpectedList, $oTBEUser->getTBEEvidenceList());
 
         $this->_hasDependencies = true;
-
         return $oTBEUser;
     }
 
     /**
+     * Test user country id caching. Second time country id is returned, it should not be recalculated.
+     * Country id must be stored in session, not in local cache.
+     *
      * @param oeVATTBETBEUser $oTBEUser
      *
      * @depends testTBEEvidenceListCaching
@@ -155,36 +198,49 @@ class Unit_oeVatTbe_models_oeVATTBETBEUserTest extends OxidTestCase
     public function testTBECountryIdCaching($oTBEUser)
     {
         $this->assertEquals('GermanyId', $oTBEUser->getTbeCountryId());
+        $this->_hasDependencies = true;
         return $oTBEUser;
     }
 
     /**
+     * Test used evidence caching. Second time evidence is returned, it should not be recalculated.
+     * Evidence must be stored in session, not in local cache.
+     *
      * @param oeVATTBETBEUser $oTBEUser
      *
-     * @depends testTBEEvidenceListCaching
+     * @depends testTBECountryIdCaching
+     *
+     * @return oeVATTBETBEUser
      */
     public function testTBEEvidenceUsedCaching($oTBEUser)
     {
         $this->assertEquals('billing_country', $oTBEUser->getTbeEvidenceUsed());
+        $this->_hasDependencies = true;
+        return $oTBEUser;
     }
 
     /**
+     * Country id should be recalculated when it is unset from cache.
+     *
      * @param oeVATTBETBEUser $oTBEUser
      *
-     * @depends testTBECountryIdCaching
+     * @depends testTBEEvidenceUsedCaching
      */
     public function testUnsetEvidenceListCaching($oTBEUser)
     {
         $oTBEUser->unsetTbeCountryFromCaching();
-        $this->assertEquals('LithuaniaId', $oTBEUser->getTbeCountryId());
+        $this->assertEquals('', $oTBEUser->getTbeCountryId());
     }
 
+    /**
+     * Country should be cached even when it is not found. It should not be rechecked on every request.
+     */
     public function testCountryIdCachingWhenCountryIsNotFound()
     {
         $oConfig = $this->getConfig();
         $oSession = $this->getSession();
         $oSession->setVariable('TBECountryId', null);
-        $oConfig->setConfigParam('blOeVATTBECountryEvidences', array('oeVATTBEBillingCountryEvidence'));
+        $oConfig->setConfigParam('aOeVATTBECountryEvidences', array('oeVATTBEBillingCountryEvidence'));
         $oConfig->setConfigParam('sOeVATTBEDefaultEvidence', 'billing_country');
 
         $oUser = oxNew('oxUser');
@@ -197,7 +253,5 @@ class Unit_oeVatTbe_models_oeVATTBETBEUserTest extends OxidTestCase
         $this->assertEquals('', $oTBEUser->getTbeCountryId());
 
         $this->_hasDependencies = true;
-
-        return $oTBEUser;
     }
 }
