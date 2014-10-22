5e135f9523d4bd6d81f88cfbc85dab361f05e73c
shiftas <Shiftass@gmail.com>
2014-10-22 11:29:07 +0300

    Adding integration test for groupsList.

---
+++ tests/integration/oevattbe/vatgroups/oevattbecountryvatgroupslistTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * This file is part of OXID eSales VAT TBE module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales VAT TBE module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ */
+
+
+/**
+ * Testing oeVATTBECountryVATGroupsList class.
+ * @covers oeVATTBECountryVATGroupsList
+ */
+class Integration_oeVatTbe_VATGroups_oeVATTBECountryVATGroupsListTest extends OxidTestCase
+{
+    /**
+     * Two Country Groups exits;
+     * List is successfully loaded and array of groups is returned.
+     */
+    public function testLoadingGroupsListWhenGroupsExists()
+    {
+        $aGroupData = array(
+            'OEVATTBE_COUNTRYID' => '8f241f11095410f38.37165361',
+            'OEVATTBE_NAME' => 'Group Name',
+            'OEVATTBE_DESCRIPTION' => 'Some description',
+            'OEVATTBE_RATE' => '20.50',
+            'OEVATTBE_TIMESTAMP' => '2014-10-22 11:19:20',
+        );
+
+        /** @var oeVATTBEVATGroupsDbGateway $oGateway */
+        $oGateway = oxNew('oeVATTBEVATGroupsDbGateway');
+
+        $oGroup1 = $this->_createGroupObject($aGroupData, $oGateway);
+        $oGroup2 = $this->_createGroupObject($aGroupData, $oGateway);
+
+        /** @var oeVATTBECountryVATGroupsList $oGroupsList */
+        $oGroupsList = oxNew('oeVATTBECountryVATGroupsList', $oGateway);
+
+        $this->assertEquals(array($oGroup1, $oGroup2), $oGroupsList->load('8f241f11095410f38.37165361'));
+    }
+
+    /**
+     * No Country Groups exits;
+     * List is successfully loaded and empty array is returned.
+     */
+    public function testLoadingGroupsListWhenNoGroupsExists()
+    {
+        /** @var oeVATTBEVATGroupsDbGateway $oGateway */
+        $oGateway = oxNew('oeVATTBEVATGroupsDbGateway');
+
+        /** @var oeVATTBECountryVATGroupsList $oGroupsList */
+        $oGroupsList = oxNew('oeVATTBECountryVATGroupsList', $oGateway);
+
+        $this->assertEquals(array(), $oGroupsList->load('NonExistingCountryId'));
+    }
+
+    /**
+     * Creates VAT Group object and sets given data to it.
+     *
+     * @param array                      $aData
+     * @param oeVATTBEVATGroupsDbGateway $oGateway
+     *
+     * @return oeVATTBEVATGroup
+     */
+    protected function _createGroupObject($aData, $oGateway)
+    {
+        /** @var oeVATTBEVATGroup $oGroup */
+        $oGroup = oxNew('oeVATTBEVATGroup', $oGateway);
+        $oGroup->setId($aData['OEVATTBE_ID']);
+        $oGroup->setData($aData);
+        $aData['OEVATTBE_ID'] = $oGroup->save();
+        $oGroup->setData($aData);
+
+        return $oGroup;
+    }
+}
