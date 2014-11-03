3b37ca6b3a929fa35cf228102613ef16ff02cf52
mantasvaitkunas <manvait@gmail.com>
2014-11-03 14:56:59 +0200

    OEVATTBE-41 Hook to events to update articles TBE information
    Added tests for dbgateway

--- tests/integration/oevattbe/vatgroups/oevattbecategoryvatgroupspopulatordbgatewayTest.php
+++ tests/integration/oevattbe/vatgroups/oevattbecategoryvatgroupspopulatordbgatewayTest.php
@@ -27,24 +27,11 @@
 class Integration_oeVatTbe_VATGroups_oeVATTBECategoryVATGroupsPopulatorDbGatewayTest extends OxidTestCase
 {
     /**
-     * Tear down.
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        oxDb::getDb()->execute('TRUNCATE TABLE `oevattbe_articlevat`');
-        oxDb::getDb()->execute('TRUNCATE TABLE `oevattbe_categoryvat`');
-        oxDb::getDb()->execute('TRUNCATE TABLE `oxobject2category`');
-        oxDb::getDb()->execute('TRUNCATE TABLE `oxcategories`');
-        oxDb::getDb()->execute('TRUNCATE TABLE `oxarticles`');
-    }
-
-    /**
      * Test populate not existing category data.
      */
     public function testPopulateNotExistingCategory()
     {
+        $this->_cleanData();
         $this->_prepareData();
 
         $oDbGateway = oxNew('oeVATTBECategoryVATGroupsPopulatorDbGateway');
@@ -57,12 +44,14 @@ class Integration_oeVatTbe_VATGroups_oeVATTBECategoryVATGroupsPopulatorDbGateway
         $this->assertEquals(0, $iRecordCount);
     }
 
-
     /**
      * Test populate existing category data.
+     *
+     * @return oeVATTBECategoryVATGroupsPopulatorDbGateway
      */
     public function testPopulateExistingCategory()
     {
+        $this->_cleanData();
         $this->_prepareData();
 
         $oDbGateway = oxNew('oeVATTBECategoryVATGroupsPopulatorDbGateway');
@@ -72,12 +61,58 @@ class Integration_oeVatTbe_VATGroups_oeVATTBECategoryVATGroupsPopulatorDbGateway
         $this->assertEquals(4, $iRecordCount);
         $iRecordCount = oxDb::getDb()->getOne("SELECT COUNT(*) FROM `oxarticles` WHERE  `oevattbe_istbeservice` = '1'");
         $this->assertEquals(2, $iRecordCount);
+
+        return $oDbGateway;
+    }
+
+    /**
+     * Checks if reset works correctly for articles.
+     *
+     * @param oeVATTBECategoryVATGroupsPopulatorDbGateway $oDbGateway
+     *
+     * @depends testPopulateExistingCategory
+     */
+    public function testResetArticles($oDbGateway)
+    {
+        $aArticles = array(
+            'article2'
+        );
+
+        $this->assertTrue($oDbGateway->reset($aArticles));
+        $iRecordCount = oxDb::getDb()->getOne("SELECT COUNT(*) FROM `oevattbe_articlevat`");
+        $this->assertEquals(2, $iRecordCount);
+        $iRecordCount = oxDb::getDb()->getOne("SELECT COUNT(*) FROM `oxarticles` WHERE  `oevattbe_istbeservice` = '1'");
+        $this->assertEquals(1, $iRecordCount);
+    }
+
+    /**
+     * Tests when empty articles array is given.
+     */
+    public function testResetArticlesWhenGivenEmptyArray()
+    {
+        $aArticles = array();
+        /** @var oeVATTBECategoryVATGroupsPopulatorDbGateway $oDbGateway */
+        $oDbGateway = oxNew('oeVATTBECategoryVATGroupsPopulatorDbGateway');
+
+        $this->assertFalse($oDbGateway->reset($aArticles));
+    }
+
+    /**
+     * Truncates database tables.
+     */
+    private function _cleanData()
+    {
+        oxDb::getDb()->execute('TRUNCATE TABLE `oevattbe_articlevat`');
+        oxDb::getDb()->execute('TRUNCATE TABLE `oevattbe_categoryvat`');
+        oxDb::getDb()->execute('TRUNCATE TABLE `oxobject2category`');
+        oxDb::getDb()->execute('TRUNCATE TABLE `oxcategories`');
+        oxDb::getDb()->execute('TRUNCATE TABLE `oxarticles`');
     }
 
     /**
      * Prepare data for test case.
      */
-    protected function _prepareData()
+    private function _prepareData()
     {
         $aSqlQueries = array();
         $aSqlQueries[] = "INSERT INTO `oevattbe_categoryvat` SET `OEVATTBE_CATEGORYID` = 'categoryId', `OEVATTBE_COUNTRYID` = 'a7c40f631fc920687.20179984', `OEVATTBE_VATGROUPID` = '10'";
