06038907632bd39a4d0b8b2710a24e781e681458
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-11-06 12:58:58 +0200

    translation updated

--- source/modules/oe/oevattbe/translations/de/oevatbte_lang.php
+++ source/modules/oe/oevattbe/translations/de/oevatbte_lang.php
@@ -25,9 +25,9 @@
 $aLang = array(
     'charset'                                         => 'ISO-8859-15',
     'PLUS_SHIPPING2'                                  => 'Versandkosten ** Elektronische Dienstleistung',
-    'OEVATTBE_ERROR_MESSAGE_TBE_ARTICLE_VAT_PROBLEMS' => 'Es war leider nicht möglich, die MwSt für einige Artikel in Ihrem Warenkorb zu berechnen und können daher nicht verkauft werden. Bitte entfernen Sie diese bitte aus dem Warenkorb: %s.',
+    'OEVATTBE_ERROR_MESSAGE_TBE_ARTICLE_VAT_PROBLEMS' => 'Es war leider nicht möglich, die MwSt. für einige Artikel in Ihrem Warenkorb zu berechnen. Sie können daher nicht verkauft werden. Bitte entfernen Sie diese aus dem Warenkorb: %s.',
     'OEVATTBE_VAT_CALCULATED_BY_USER_COUNTRY'         => 'Diese MwSt. wurde für Ihren Standort berechnet: %s.',
     'OEVATTBE_VAT_WILL_BE_CALCULATED_BY_USER_COUNTRY' => 'Die MwSt. ist abhängig vom Kundenstandort (aktuell %s) und kann sich nach der Anmeldung ändern.',
-    'OEVATTBE_RESIDENCE_COUNTRY_CHANGED_MESSAGE'      => 'Ihr Standort wurde als %s identifiziert. Dieser Standort wird in der MwST.-Berechnung für z.B. eBooks oder MP3s verwandt',
+    'OEVATTBE_RESIDENCE_COUNTRY_CHANGED_MESSAGE'      => 'Ihr Standort wurde als %s identifiziert. Dieser Standort wird in der MwSt.-Berechnung für z.B. eBooks oder MP3s verwandt.',
     'OEVATTBE_VAT_EXCLUDED'                           => 'MwSt. wurde nicht berechnet.',
 );
