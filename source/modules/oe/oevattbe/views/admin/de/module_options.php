97531d4f4950d1984c27147609d2b68978d861f2
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-11-10 13:07:45 +0200

    translation updated

--- source/modules/oe/oevattbe/views/admin/de/module_options.php
+++ source/modules/oe/oevattbe/views/admin/de/module_options.php
@@ -24,10 +24,10 @@
 // -------------------------------
 $aLang = array(
     'charset'                                        => 'ISO-8859-15',
-    'SHOP_MODULE_GROUP_oevattbe'                     => 'eVAT Kundenstandorts-Bestimmungsmethode',
-    'SHOP_MODULE_aOeVATTBECountryEvidences'          => 'Kundenstandorts-Bestimmungsmethode aktivieren oder deaktivieren',
+    'SHOP_MODULE_GROUP_oevattbe'                     => 'Bestimmungsmethode für Kundenstandort',
+    'SHOP_MODULE_aOeVATTBECountryEvidences'          => 'Bestimmungsmethode für Kundenstandort aktivieren oder deaktivieren',
     'HELP_SHOP_MODULE_aOeVATTBECountryEvidences'     => 'Eine Kundenstandorts-Bestimmungmethode kann als aktiv (1) oder inaktiv (0) markiert werden. Nur aktive Kundenstandorts-Bestimmungmethoden werden in der Kundenstandorts-Bestimmung benutzt. Bitte löschen Sie keine Einträge aus dieser Liste - ändern Sie nur die Ziffern "0" oder "1" wie benötigt.',
-    'SHOP_MODULE_sOeVATTBEDefaultEvidence'           => 'Default Kundenstandorts-Bestimmungmethode',
+    'SHOP_MODULE_sOeVATTBEDefaultEvidence'           => 'Standard-Bestimmungsmethode für Kundenstandort',
     'HELP_SHOP_MODULE_sOeVATTBEDefaultEvidence'      => 'Diese Methode wird zur Standortbestimmung benutzt. Sie muss aktiv sein.',
     'SHOP_MODULE_sOeVATTBEDomesticCountry'           => 'Sitz des Shops',
     'HELP_SHOP_MODULE_sOeVATTBEDomesticCountry'      => 'ISO2-Kürzel des Landes, in dem der Shop bzw. die Firma ansässig ist.',
