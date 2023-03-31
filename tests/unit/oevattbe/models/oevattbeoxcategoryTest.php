df60ea26b366ee5f13037f4a0150ce09ca3e3d04
Angel Dimitrov <angel.dimitrov@dnhsoft.com>
2023-03-31 09:09:46 +0300

    OXDEV-6892 Update file headers

--- tests/unit/oevattbe/models/oevattbeoxcategoryTest.php
+++ tests/unit/oevattbe/models/oevattbeoxcategoryTest.php
@@ -1,24 +1,8 @@
 <?php
 /**
- * This file is part of OXID eSales eVAT module.
- *
- * OXID eSales eVAT module is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * OXID eSales eVAT module is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with OXID eSales eVAT module.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2014
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
  */
-
 
 /**
  * Testing extended oxCategory class.
