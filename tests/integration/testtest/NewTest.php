5ebf21a7064d139ab51ec4167dd7acb7bbdc3bee
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-04-20 16:07:29 +0200

    OXDEV-6944: Implement own Test to test github actions

---
+++ tests/integration/testtest/NewTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OxidEsales\EVat\Tests\Integration;
+
+class NewTest extends \OxidEsales\EshopCommunity\Tests\Integration\IntegrationTestCase
+{
+    public function testCreateDeliveryAddressWithMissingFields(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/unit/testtest/NewTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace OxidEsales\EVat\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class NewTest extends TestCase
+{
+    public function testCreateDeliveryAddressWithMissingFields(): void
+    {
+        $this->assertTrue(true);
+    }
+}
