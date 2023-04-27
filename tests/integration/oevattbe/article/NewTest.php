044d8b850c4436c12afb425dab38722d332defbe
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-04-27 10:46:56 +0200

    OXDEV-6944: Move test to another directory and use group to test them

--- tests/integration/oevattbe/article/NewTest.php
+++ tests/integration/oevattbe/article/NewTest.php
@@ -4,6 +4,10 @@ namespace OxidEsales\EVat\Tests\Integration;
 
 class NewTest extends \OxidEsales\EshopCommunity\Tests\Integration\IntegrationTestCase
 {
+    /**
+     * @return void
+     * @group foobar
+     */
     public function testCreateDeliveryAddressWithMissingFields(): void
     {
         $this->assertTrue(true);
--- tests/unit/oevattbe/components/NewTest.php
+++ tests/unit/oevattbe/components/NewTest.php
@@ -7,6 +7,10 @@ use PHPUnit\Framework\TestCase;
 
 class NewTest extends TestCase
 {
+    /**
+     * @return void
+     * @group foobar
+     */
     public function testCreateDeliveryAddressWithMissingFields(): void
     {
         $this->assertTrue(true);
